6a3332d155f07ad8636f2fb1c85c7002473d49f4
Jerin <jerin.t@gmail.com>
2025-02-22 21:41:23 +0530

    Strongly Typed IDs, Rich domain model entity and domain events

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Exceptions
+{
+    class DomainException : Exception
+    {
+        public DomainException(string message) 
+            : base($"Domain exception: {message} thrown from Domain layer")
+        {
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,13 +1,11 @@
-﻿
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models
 {
-    public class Order : Aggregate<Guid>
+    public class Order : Aggregate<OrderId>
     {
         private readonly List<OrderItem> _orderItems = new();
         public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-        public Guid CustomerId { get; private set; } = default!;
-        public string OrderName { get; private set; } = default!;
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
@@ -17,6 +15,72 @@ namespace Ordering.Domain.Models
         {
             get { return _orderItems.Sum(item => item.Price); }
             private set { }
+        }
+
+
+        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, 
+            Address billingAddress, Payment payment)
+        {
+            //ArgumentException.ThrowIfNull(customerId);
+            //ArgumentException.ThrowIfNull(orderName);
+            //ArgumentException.ThrowIfNull(shippingAddress);
+            //ArgumentException.ThrowIfNull(billingAddress);
+            //ArgumentException.ThrowIfNull(payment);
+            var order = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending
+            };
+
+            order.AddDomainEvent(new OrderCreatedEvent(order));
+
+            return order;
+        }
+
+        public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+        {
+           
+            OrderName = orderName;
+            ShippingAddress = shippingAddress;
+            BillingAddress = billingAddress;
+            Payment = payment;
+            Status = status;
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+
+        public void Add(ProductId productId, decimal price, int quantity)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var orderItem = new OrderItem(Id, productId, price, quantity);
+            _orderItems.Add(orderItem);
+
+            //var existingOrderForProduct = _orderItems.FirstOrDefault(o => o.ProductId == productId);
+            //if (existingOrderForProduct != null)
+            //{
+            //    existingOrderForProduct.AddUnits(quantity);
+            //}
+            //else
+            //{
+            //    var orderItem = new OrderItem(Id, productId, price, quantity);
+            //    _orderItems.Add(orderItem);
+            //}
+        }
+
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(o => o.ProductId == productId);
+            if (orderItem != null)
+            {
+                _orderItems.Remove(orderItem);
+            }
+            
         }
 
     }
