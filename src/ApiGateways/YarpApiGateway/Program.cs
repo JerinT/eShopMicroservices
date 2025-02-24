6177e3ee02b23fcf979d1dfac5529e73ff8a74d8
Jerin <jerin.t@gmail.com>
2025-02-24 20:28:17 +0530

    yarp setup with routing and rate limiting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 
 var app = builder.Build();
 
-//configure http request
+//configure http
+app.UseRateLimiter();
+
+app.MapReverseProxy();
 
 app.Run();
